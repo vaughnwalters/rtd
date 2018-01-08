4a8c7a5d4d9a1ac4e18e1afd3471e0b0efcd902e
Vaughn Walters <vaughnwalters@gmail.com>
2018-01-07 23:10:48 -0600

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import List from './List'
 
-// import WILogo from './WILogo.png';
-import List from './List';
-
-// console.log(Component)
 const initialList = (typeof localStorage['itemArr'] !== 'undefined') ? JSON.parse(localStorage.getItem('itemArr')) : []
 
 class App extends Component {
@@ -19,14 +16,10 @@ class App extends Component {
   }
 
   onChange = (e) => {
-    // console.log('event', e)
-    // changes state every time a letter is entered
     this.setState({ item: e.target.value })
   }
 
   onSubmit = (e) => {
-    // console.log('event', e.target.value)
-    // prevent default keeps page from reloading
     e.preventDefault()
     const itemArr = [...this.state.itemArr, this.state.item]
     localStorage.setItem('itemArr', JSON.stringify(itemArr)) 
@@ -36,30 +29,22 @@ class App extends Component {
     })
   }
 
-
   delete = (id) => {
-    // console.log(id)
     console.log(this.state.itemArr)
     const itemArr = this.state.itemArr.filter(el => el !== id)
     console.log(itemArr)
     localStorage.setItem('itemArr', JSON.stringify(itemArr)) 
-    // 
     this.setState({
       itemArr: itemArr
-    
-    });
+    })
   }
 
   render() {
     return (
       <div className="App">
-        {/*<header>
-          <img src={WILogo} alt='logo' />
-          <h1 className='App-title'>Todo App</h1>
-        </header> */}
-         
+
         <form onSubmit={this.onSubmit}>
-          <input placeholder="Enter a task" value={this.state.item} onChange={this.onChange} />
+          <input spellcheck="false" placeholder="Enter a task" value={this.state.item} onChange={this.onChange} />
           <button>Add</button>
         </form>
 
--- src/List.js
+++ src/List.js
@@ -1,18 +1,9 @@
 import React from 'react'
-// import ListItem from './ListItem'
-
-  // deleteItem = (e) => {
-  //   this.setState({ 
-  //     itemArr: []
-  //   })
-  // }
-
 
 const List = props => (
   <ul>
     {
       props.itemArr.map((item, index) => <li className="list-item"key={index}> {item} 
-        {/* <ListItem /> */}
         <button onClick={props.delete.bind(this, item)}>x</button>
         </li>
       )
