b34f69eb3cf473640c9c6093e32fb82fdbf6e5da
Vaughn Walters <vaughnwalters@gmail.com>
2018-01-10 19:34:55 -0600

    working more on refactor into redux

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 
 
+
 const initialList = (typeof localStorage['itemArr'] !== 'undefined') ? JSON.parse(localStorage.getItem('itemArr')) : []
 
-
+// FUNCTIONAL COMPONENT
 const List = props => (
   <ul>
     {
       props.itemArr.map((item, index) => <li className="list-item"key={index}> {item} 
         {/* <ListItem /> */}
-        <button className="delete" onClick={props.delete.bind(this, item)}>-</button>
+        <button
+          className="delete"
+          onClick={props.delete.bind(this, index)}>-</button>
         </li>
       )
     }
@@ -36,48 +39,59 @@ const List = props => (
 
 // TODO COMPONENT
 class Todo extends Component {
-   constructor(props) {
-    super(props)
-    this.state = {
-      item: '', 
-      itemArr: initialList
-    }
-    this.delete = this.delete.bind(this)
-  }
+  //  constructor(props) {
+  //   super(props)
+  //   // this.state = {
+  //   //   item: '', 
+  //   //   itemArr: initialList
+  //   // }
+  //   // this.delete = this.delete.bind(this)
+  // }
+
+  // delete() {}
 
   onChange = (e) => {
-    this.setState({ item: e.target.value })
+    // this.setState({ item: e.target.value })
+    this.props.typing(e.target.value)
   }
 
   onSubmit = (e) => {
     e.preventDefault()
-    const itemArr = [...this.state.itemArr, this.state.item]
-    localStorage.setItem('itemArr', JSON.stringify(itemArr)) 
-    this.setState({
-      item: '',
-      itemArr: itemArr
-    })
+    // const itemArr = [...this.state.itemArr, this.state.item]
+    // localStorage.setItem('itemArr', JSON.stringify(itemArr)) 
+    // this.setState({
+    //   item: '',
+    //   itemArr: itemArr
+    // })
+    this.props.submitAction()
   }
 
-  delete = (id) => {
-    console.log(this.state.itemArr)
-    const itemArr = this.state.itemArr.filter(el => el !== id)
-    console.log(itemArr)
-    localStorage.setItem('itemArr', JSON.stringify(itemArr)) 
-    this.setState({
-      itemArr: itemArr
-    });
-  }
+  // delete = (index) => {
+  //   // console.log(this.state.itemArr)
+  //   const itemArr = this.state.itemArr.filter(el => el !== id)
+  //   console.log(itemArr)
+  //   // abstract this implementation elsewhere
+  //   localStorage.setItem('itemArr', JSON.stringify(itemArr))
+  //   // redux-logic
+  //   // (logic middleware that listens to delete and then handles state mutations like local storage, etc)
+  //   // this.setState({
+  //   //   itemArr: itemArr
+  //   // });
+  //   this.props.delete()
+  // }
 
   render() {
     return (
       <div className="App">
         <form onSubmit={this.onSubmit}>
-          <input spellCheck="false" placeholder="Enter a task" value={this.state.item} onChange={this.onChange} />
+          <input spellCheck="false" placeholder="Enter a task" value={this.props.item} onChange={this.onChange} />
           <button className="addTask">+</button>
         </form>
 
-        <List itemArr={this.state.itemArr} delete={this.delete} />
+        <List
+          itemArr={this.props.itemArr}
+          delete={this.props.deleteAction}
+        />
 
       </div>
     )
@@ -89,17 +103,22 @@ class Todo extends Component {
 // const decreaseAction = { type: 'decrease' }
 
 // Todo Action
-const submitAction = () => {
- type: 'increase' 
-}
+const submitAction = () => ({
+ type: 'CREATE_TODO'
+})
+
+const typing = (value) => ({
+  type: 'DESCRIBE_TODO',
+  payload: value
+})
 
 // refactor from onChange
 // const 
-
-const deleteAction = (id) =>{ 
-  type: 'decrease',
-  id 
-}
+// not currrently removing from localStorage
+const deleteAction = (index) => ({ 
+  type: 'DELETE_TODO',
+  payload: index 
+})
 
 
 // Counter Reducer
@@ -115,24 +134,41 @@ const deleteAction = (id) =>{
 //   }
 // }
 
+const initialState = {item: '', itemArr: []}
 // Todo Reducer
-function todoReducer(state = {
-      item: '', 
-      itemArr: initialList
-    }, action) {
-  const count = state.count
-  console.log(count)
+function todoReducer(state = initialState, action) {
+  // const count = state.count
+  // console.log(count)
   switch (action.type) {
-    case 'submitAction':
+    case 'DESCRIBE_TODO':
+      return {
+        ...state,
+        item: action.payload
+      }
+    case 'CREATE_TODO':
       // return { itemArr: count + 1 }
-      return deleteAction
-    case 'deleteAction':
+      // return deleteAction
+      // console.log(state.item)
+      return {
+        item: '',
+        itemArr: [...state.itemArr, state.item]
+      }
+    case 'DELETE_TODO':
       // return { count: count - 1 }
-      return deleteAction
-           default:
+      const index = action.payload
+      return {
+        ...state,
+        itemArr: [
+          ...state.itemArr.slice(0, index), // figure out math
+          ...state.itemArr.slice(index + 1)
+        ]
+      }
+    default:
       return state
   }
 }
+
+// on @@APP_INIT or similar load saved todos from local storage
 
 // Counter Store
 // const store = createStore(counter)
@@ -149,11 +185,14 @@ const store = createStore(todoReducer)
 // }
 
 // Todo Map Redux state to component props
+
+// this.props.item and then value is state.item, por ejamplo
 function mapStateToProps(state) {
   console.log('state', state)
   return {
       item: state.item, 
-      itemArr: initialList
+      // itemArr: initialList
+      itemArr: state.itemArr
   }
 }
 
@@ -166,12 +205,26 @@ function mapStateToProps(state) {
 
 
 // Todo Map Redux actions to component props
-function mapDispatchToProps(dispatch) {
-  console.log(dispatch)
-  return {
-    onSubmit: () => dispatch(submitAction),
-    delete: () => dispatch(deleteAction)
-  }
+// function mapDispatchToProps(dispatch) {
+//   console.log(dispatch)
+//   return {
+//     onSubmit: () => dispatch(submitAction),
+//     delete: () => dispatch(deleteAction)
+//   }
+// }
+
+// method on the store to consume the action object
+
+// passing actions we created (in this example keys with the same names)
+
+// mapDispatchToProps if an object is automatically bound to Dispatch, so each key inside the object becomes bound to dispatch.  If a function though, need to be explicit about it 
+
+const mapDispatchToProps = {
+  // onSubmit: submitAction
+  // delete: deleteAction
+  submitAction,
+  deleteAction,
+  typing
 }
 
 // // Counter Connected Component
@@ -205,8 +258,9 @@ ReactDOM.render(
 
 
 
-
-
+// mapStateToProps -
+// first arg is a function that takes state as an argument
+// can call state X and props Y 
 
 
 
